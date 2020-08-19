e0a980b5160ad33b18094aee7daba417c1d607cb
rafaelanastacioalves <rafaelanastacioalves@gmail.com>
2020-08-19 20:06:55 -0300

    fix: ViewPaging - improves footer view when achieving the end of the tab view

--- app/src/main/java/com/rafaelanastacioalves/design/concepts/ui/viewpaging/TabForViewPagerAdapter.kt
+++ app/src/main/java/com/rafaelanastacioalves/design/concepts/ui/viewpaging/TabForViewPagerAdapter.kt
@@ -3,13 +3,20 @@ package com.rafaelanastacioalves.design.concepts.ui.viewpaging
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.marginEnd
+import androidx.core.view.marginStart
 import androidx.recyclerview.widget.RecyclerView
 import com.rafaelanastacioalves.design.concepts.R
 import com.rafaelanastacioalves.design.concepts.listeners.RecyclerViewClickListener
 import kotlinx.android.synthetic.main.viewpager_recyclerview_tab_viewholder.view.*
 
 class TabForViewPagerAdapter(val recyclerViewClickListener: RecyclerViewClickListener) : RecyclerView.Adapter<TabViewHolder>() {
+
+    var viewHolderWidth: Int = 0
+    var viewHolderOffSet: Int = 0
     var selectedItemIndex: Int = 0
+
+    enum class ViewHolderType {NORMAL, FOOTER_VIEW}
     lateinit var tabList: List<TabItemElement>
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TabViewHolder {
         return TabViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.viewpager_recyclerview_tab_viewholder, parent, false))
@@ -19,27 +26,43 @@ class TabForViewPagerAdapter(val recyclerViewClickListener: RecyclerViewClickLis
         return if (tabList.isNullOrEmpty()) {
             0
         } else {
-            tabList.size
+            tabList.size + 2
         }
     }
 
     override fun onBindViewHolder(holder: TabViewHolder, position: Int) {
-        if (selectedItemIndex == position){
-            holder.itemView.textView.text = tabList[position].textPage + "*"
-        }else{
-            holder.itemView.textView.text = tabList[position].textPage
-        }
+        if (holder.itemViewType == ViewHolderType.NORMAL.ordinal) {
+            if (selectedItemIndex == position){
+                holder.itemView.textView.text = tabList[position].textPage + "*"
+            }else{
+                holder.itemView.textView.text = tabList[position].textPage
+            }
 
-        if (tabList[position].hasSelectinos){
-            var text = holder.itemView.textView.text
-            holder.itemView.textView.text = text.toString() + "[X]"
+            if (tabList[position].hasSelectinos){
+                var text = holder.itemView.textView.text
+                holder.itemView.textView.text = text.toString() + "[X]"
+            }
+            holder.itemView.setOnClickListener({ v ->
+                recyclerViewClickListener.onClick(v, position)
+                selectedItemIndex = position
+            })
         }
-        holder.itemView.setOnClickListener({ v ->
-            recyclerViewClickListener.onClick(v, position)
-            selectedItemIndex = position
-        })
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return if (position < tabList.size) ViewHolderType.NORMAL.ordinal else ViewHolderType.FOOTER_VIEW.ordinal
+
+    }
+
+    override fun onViewAttachedToWindow(holder: TabViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        viewHolderWidth =  if (viewHolderWidth == 0)  holder.itemView.measuredWidth else viewHolderWidth
+        viewHolderOffSet = if (viewHolderOffSet == 0) holder.itemView.let { it.marginStart + it.marginEnd } else viewHolderOffSet
+        if(holder.itemViewType == ViewHolderType.FOOTER_VIEW.ordinal){
+            holder.itemView.visibility = View.INVISIBLE
+            holder.itemView.minimumWidth = 200
+        }
+    }
 }
 
 class TabViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/com/rafaelanastacioalves/design/concepts/ui/viewpaging/ViewPagerActivity.kt
+++ app/src/main/java/com/rafaelanastacioalves/design/concepts/ui/viewpaging/ViewPagerActivity.kt
@@ -1,33 +1,37 @@
 package com.rafaelanastacioalves.design.concepts.ui.viewpaging
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginEnd
+import androidx.core.view.marginStart
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.*
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.rafaelanastacioalves.design.concepts.R
 import com.rafaelanastacioalves.design.concepts.listeners.RecyclerViewClickListener
+import kotlinx.android.synthetic.main.home_activity.*
 import kotlinx.android.synthetic.main.viewpager_activity.*
 import kotlinx.android.synthetic.main.viewpager_item_viewholder.view.*
 
 class ViewPagerActivity : AppCompatActivity() {
     val list = generateTabItemViewHolderData()
-    lateinit var tabForViewPagerAdapter : TabForViewPagerAdapter
-    lateinit var viewPagerAdapter : ViewPagerAdapter
+    lateinit var tabForViewPagerAdapter: TabForViewPagerAdapter
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    var totalDxScroll: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.viewpager_activity)
         setupViewPager()
-        setupViewPagerTabWithRecyclerView()
+        setupTabForRecyclerView()
 
     }
 
-    private fun setupViewPagerTabWithRecyclerView() {
+    private fun setupTabForRecyclerView() {
         tabForViewPagerAdapter = TabForViewPagerAdapter(object : RecyclerViewClickListener {
             override fun onClick(view: View, position: Int) {
                 view_pager.setCurrentItem(position, true)
@@ -38,6 +42,13 @@ class ViewPagerActivity : AppCompatActivity() {
         val linearLayoutManager = LinearLayoutManager(this)
         linearLayoutManager.orientation = HORIZONTAL
         viewpager_tab_recyclerview.layoutManager = linearLayoutManager
+        viewpager_tab_recyclerview.suppressLayout(true)
+        viewpager_tab_recyclerview.addOnScrollListener(object : OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                totalDxScroll = +dx
+            }
+        })
     }
 
     private fun generateTabItemViewHolderData(): List<TabItemElement> {
@@ -59,11 +70,31 @@ class ViewPagerActivity : AppCompatActivity() {
         viewPagerAdapter.adapterlist = list
         view_pager.adapter = viewPagerAdapter
         view_pager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            var currentPosition = 0
+            var currentAbsoluteOffset = 0
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 tabForViewPagerAdapter.selectedItemIndex = position
-                viewpager_tab_recyclerview.scrollToPosition(position)
                 tabForViewPagerAdapter.notifyDataSetChanged()
+
+            }
+
+            private fun calculateDx(position: Int): Int {
+
+                var dx = 0
+                dx =+ (tabForViewPagerAdapter.viewHolderWidth + tabForViewPagerAdapter.viewHolderOffSet)*(position)
+                return dx
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                if (position != currentPosition){
+                    viewpager_tab_recyclerview.animateToPosition(position, calculateDx(
+                            if (position > currentPosition) 1 else -1))
+                    currentPosition = position
+
+                }
+
 
             }
         })
@@ -79,16 +110,23 @@ class ViewPagerActivity : AppCompatActivity() {
 
 }
 
+private fun RecyclerView.animateToPosition(position: Int, dx: Int) {
+    suppressLayout(false)
+    (layoutManager as LinearLayoutManager).scrollToPositionWithOffset(position, 20)
+    suppressLayout(true)
+//    scrollBy(dx, 0)
+}
+
 class ViewPagerAdapter(val recyclerViewClickListener: RecyclerViewClickListener) : RecyclerView.Adapter<SampleViewHolder>() {
 
-    lateinit var adapterlist : List<TabItemElement>
+    lateinit var adapterlist: List<TabItemElement>
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SampleViewHolder {
-        return SampleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.viewpager_item_viewholder,parent, false))
+        return SampleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.viewpager_item_viewholder, parent, false))
     }
 
 
     override fun getItemCount(): Int {
-        return if(adapterlist.isNullOrEmpty()){
+        return if (adapterlist.isNullOrEmpty()) {
             0
         }else{
             adapterlist.size
@@ -96,11 +134,15 @@ class ViewPagerAdapter(val recyclerViewClickListener: RecyclerViewClickListener)
     }
 
     override fun onBindViewHolder(holder: SampleViewHolder, position: Int) {
+
+
         holder.itemView.textView.text = adapterlist.get(position).textPage
         holder.itemView.button.setOnClickListener({ v-> recyclerViewClickListener.onClick(
                 v, position
         ) })
     }
+
+
 
 }
 
