35a6067b96709a67ace5892dd9ff08fd58db8e19
rafaelanastacioalves <rafaelanastacioalves@gmail.com>
2020-10-09 22:52:31 -0300

    fix: Util - Merge Colors fixed

--- app/src/main/java/com/rafaelanastacioalves/design/concepts/common/Utils.kt
+++ app/src/main/java/com/rafaelanastacioalves/design/concepts/common/Utils.kt
@@ -8,10 +8,10 @@ import android.graphics.Color
 class Utils {
     companion object {
         fun mergeColors(startColor: Int, endColor: Int, p: Float): Int {
-            val red: Int = (p * Color.red(startColor) + (1 - p) * Color.red(endColor)).toInt()
-            val blue: Int = (p * Color.blue(startColor) + (1 - p) * Color.blue(endColor)).toInt()
-            val green: Int = (p * Color.green(startColor) + (1 - p) * Color.green(endColor)).toInt()
-            val alpha: Int = (p * Color.alpha(startColor) + (1 - p) * Color.alpha(endColor)).toInt()
+            val red: Int = (p * Color.red(endColor) + (1 - p) * Color.red(startColor)).toInt()
+            val blue: Int = (p * Color.blue(endColor) + (1 - p) * Color.blue(startColor)).toInt()
+            val green: Int = (p * Color.green(endColor) + (1 - p) * Color.green(startColor)).toInt()
+            val alpha: Int = (p * Color.alpha(endColor) + (1 - p) * Color.alpha(startColor)).toInt()
 
             return Color.argb(alpha, red, green, blue)
         }
