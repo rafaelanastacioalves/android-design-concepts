2e3612c1e361f1a0b74ae48765a93d8dd1f0c030
rafael.alves <rafael.alves@vuxx.com.br>
2021-01-13 13:52:16 -0300

    feat: Scroll With Motion - Background and label ok

--- app/src/main/java/com/rafaelanastacioalves/design/concepts/ui/motion_layout_scroll/CollapsibleToolbar.kt
+++ app/src/main/java/com/rafaelanastacioalves/design/concepts/ui/motion_layout_scroll/CollapsibleToolbar.kt
@@ -2,6 +2,7 @@ package com.rafaelanastacioalves.design.concepts.ui.motion_layout_scroll
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.google.android.material.appbar.AppBarLayout
 
@@ -9,7 +10,9 @@ class CollapsibleToolbar @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : MotionLayout(context, attrs, defStyleAttr), AppBarLayout.OnOffsetChangedListener {
     override fun onOffsetChanged(appBarLayout: AppBarLayout?, offset: Int) {
-        progress = offset / appBarLayout?.totalScrollRange?.toFloat()!!
+        progress = -offset / appBarLayout?.totalScrollRange?.toFloat()!!
+        Log.d(javaClass.simpleName, "Progress: ${progress}  offset: ${offset}")
+
     }
 
     override fun onAttachedToWindow() {
--- app/src/main/java/com/rafaelanastacioalves/design/concepts/ui/motion_layout_scroll/ScrollWithMotion.kt
+++ app/src/main/java/com/rafaelanastacioalves/design/concepts/ui/motion_layout_scroll/ScrollWithMotion.kt
@@ -8,5 +8,16 @@ class ScrollWithMotion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.scroll_with_motion_activity)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+
     }
 }
